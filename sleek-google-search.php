4314326c5e578ca16937e9bee552a0849ae739e3
powerbuoy <powerbuoy@gmail.com>
2021-12-10 13:31:33 +0100

    Use admin textdomain

--- sleek-google-search.php
+++ sleek-google-search.php
@@ -4,8 +4,8 @@ namespace Sleek\GoogleSearch;
 ####################
 # Add settings field
 add_action('admin_init', function () {
-#	\Sleek\Settings\add_setting('google_search_api_key', 'text', __('Google Search API Key', 'sleek'));
-#	\Sleek\Settings\add_setting('google_search_engine_id', 'text', __('Google Search Engine ID', 'sleek'));
+#	\Sleek\Settings\add_setting('google_search_api_key', 'text', __('Google Search API Key', 'sleek_admin'));
+#	\Sleek\Settings\add_setting('google_search_engine_id', 'text', __('Google Search Engine ID', 'sleek_admin'));
 });
 
 # TODO...
